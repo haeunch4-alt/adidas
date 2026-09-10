226eaf3dbfbc8c81e8ed8127662ce93b8da65f5f
haeunch4-alt <haeun.ch4@gmail.com>
2026-09-10 18:00:58 +0900

    1행 js 완료 + 2행 시작

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,16 @@
 // index.js
+var mainBnrSwiper = new Swiper('.main_bnr',{
+    slidesPerView: 1,
+    navigation: {
+	nextEl: '.swiper-button-next',
+	prevEl: '.swiper-button-prev',
+    },
+    //스크롤바 표시
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+    autoplay:{
+        delay:2500,
+        disableOnInteraction:false,
+    },
+})
